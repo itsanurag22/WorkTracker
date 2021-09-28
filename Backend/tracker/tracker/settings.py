78cab79f44bade4321705402cbb894de7650aaa9
Anurag <anurag.borhade22@gmail.com>
2021-09-28 22:42:00 +0530

    add initial files

--- Backend/tracker/tracker/settings.py
+++ Backend/tracker/tracker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tracker_app',
+    'corsheaders',
     'ckeditor',
 
 ]
@@ -51,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'tracker.urls'
